64b75040322ebf9984e09d0a99ca8d27a6403e80
Austin Aspaas <araspaas@live.com>
2020-11-10 16:49:49 -0700

    Make model methods tests for bulk discount model

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -11,4 +11,36 @@ describe BulkDiscount do
   describe "associations" do
     it { should belong_to :merchant }
   end
+
+  describe "class methods" do
+    it "::find_eligible_discount()" do
+      merchant1 = create(:merchant)
+      discount3 = create(:discount, merchant: merchant1, bulk_quantity: 30, percentage_discount: 12)
+      discount1 = create(:discount, merchant: merchant1, bulk_quantity: 10, percentage_discount: 5)
+      discount2 = create(:discount, merchant: merchant1, bulk_quantity: 20, percentage_discount: 8)
+
+      expect(merchant1.bulk_discounts.find_eligible_discount(5)).to eq(nil)
+      expect(merchant1.bulk_discounts.find_eligible_discount(9)).to eq(nil)
+
+      expect(merchant1.bulk_discounts.find_eligible_discount(10)).to eq(discount1)
+      expect(merchant1.bulk_discounts.find_eligible_discount(19)).to eq(discount1)
+
+      expect(merchant1.bulk_discounts.find_eligible_discount(20)).to eq(discount2)
+      expect(merchant1.bulk_discounts.find_eligible_discount(29)).to eq(discount2)
+
+      expect(merchant1.bulk_discounts.find_eligible_discount(30)).to eq(discount3)
+    end
+  end
+
+  describe "instance methods" do
+    it "#calculate_discount()" do
+      merchant1 = create(:merchant)
+      discount1 = create(:discount, merchant: merchant1, bulk_quantity: 5, percentage_discount: 10)
+
+      bulk_order = {quantity: 5, price: 10}
+      subtotal = bulk_order[:quantity] * bulk_order[:price]
+
+      expect(discount1.calculate_discount(subtotal)).to eq(45.0)
+    end
+  end
 end
