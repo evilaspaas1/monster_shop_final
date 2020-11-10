bd80326e652702cd5f6f40913968cd52a6214ff6
Austin Aspaas <araspaas@live.com>
2020-11-10 16:53:46 -0700

    Add model methods in bulk discount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,12 @@ class BulkDiscount < ApplicationRecord
 
   validates_inclusion_of :percentage_discount, in: 1..100, message:  "Discount must be percentage from 1 - 100"
   validates :bulk_quantity, numericality: { greater_than: 0 }
+
+  def self.find_eligible_discount(item_quantity)
+    order(:bulk_quantity).where("bulk_quantity <= ?", item_quantity).last
+  end
+
+  def calculate_discount(price)
+    price * (100 - percentage_discount) * 0.01
+  end
 end
